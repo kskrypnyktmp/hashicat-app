e793d3fb03ad9897d20ddb11e8a2b5476b956da6
kskrypnyktmp <kskrypnyk.tmp@gmail.com>
2024-04-19 08:35:09 +0000

    Added s3 bucket private module

--- main.tf
+++ main.tf
@@ -198,8 +198,8 @@ resource "aws_key_pair" "hashicat" {
   public_key = tls_private_key.hashicat.public_key_openssh
 }
 
-module "s3_bucket" {
-  source              = "app.terraform.io/kskrypnykworkshop/s3-bucket/aws"
-  version             = "3.1.0"
-  s3_object_ownership = "BucketOwnerEnforced"
-}
+# module "s3_bucket" {
+#   source              = "app.terraform.io/kskrypnykworkshop/s3-bucket/aws"
+#   version             = "3.1.0"
+#   s3_object_ownership = "BucketOwnerEnforced"
+# }
